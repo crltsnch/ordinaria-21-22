dce7192ef4f63fcf74838f6cff69b765cd44d1d5
crltsnch <crltsnch@myuax.com>
2023-01-18 18:31:00 +0100

    definimos el tipo q es x e y

--- organizaciones.py
+++ organizaciones.py
@@ -1,3 +1,12 @@
 class Organizacion:
-    def __init__(self, x, y):
+    def __init__(self, x, y):   #x es el nombre e y es la lista de superheroes
+        if type(x) != str:
+            raise TypeError('El nombre debe ser un string')  #con el raise se levanta la excepción
+        if type(y) != list:
+            raise TypeError('y debe ser una lista')
+        if not y:
+            raise ValueError('La lista no puede estar vacía, debe haber al menos un superhéroe')
+        self.nombre = x
+        self.superheroes = y
+
         
